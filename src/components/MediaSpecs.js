443b77d254745faae8a136bcb1bdc4d9c438db9f
andrewbdesign <contact@andrewbdesign.com>
2018-12-04 17:43:07 +1100

    adding the other page

--- src/components/MediaSpecs.js
+++ src/components/MediaSpecs.js
@@ -2,37 +2,112 @@ import React from 'react'
 
 const MediaSpecs = () => (
   <div className="media-specs">
-    <div className='container'>
-      <h2>Rich Media Build Assets Requirements (Smart Phone / Tablets)</h2>
-      <p>Please contact TouchCreate team for more details.</p>
-      <table>
-        <tbody>
-          <tr>
-            <th>Mobile</th>
-            <th>Creative Size</th>
-            <th>PSD Assets Dimensions</th>
-            <th>Assets required</th>
-            <th>Animation length</th>
-            <th>Expandable size</th>
-            <th>File size limit</th>
-            <th>Can video be included?</th>
-            <th>Notes</th>
-          </tr>
-
-          <tr>
-            <td></td>
-            <td>320x50 (Mobile Banner)</td>
-            <td>640x100</td>
-            <td>PSD, Fonts, Storyboards. Third party Click trackers and impression trackers (if applicable)</td>
-            <td>30 secs max</td>
-            <td>Fullscreen Responsive</td>
-            <td>60kb - jpg/gif</td>
-            <td>Yes - Will autoplay with sound mute.</td>
-            <td>Please contact TouchCreate team for more detail.</td>
-          </tr>
-        </tbody>
-      </table>
+    <div className="side-menu">
+      <p>Rich Media Specifications</p>
+      <hr />
+      <h3>Mobile</h3>
+      <p>320x50 (Mobile Banner)</p>
+      <p>300x250 (Mrec)</p>
+      <p>320x480 (Mobile Interstitial)</p>
+      <hr />
+      <h3>Tablet</h3>
+      <p>728x90 (Tablet Banner)</p>
+      <p>768x1024 (Tablet Interstitial)</p>
+      <hr />
+      <h3>Video</h3>
+      <p>pre-roll/TV Sync)</p>
     </div>
+    <div className="main-section">
+      <div className='container'>
+        <h1>Smart Phone</h1>
+        <hr />
+        <h2>320x50 (Mobile Banner)</h2>
+        
+        <h3>Creative Size</h3>
+        <p>320x50 (Mobile Banner)</p>
+
+        <h3>PSD Assets dimensions to be supplied from client</h3>
+        <p>640x100</p>
+
+        <h3>Assets required</h3>
+        <p>PSD, Fonts, Storyboard. Third party click trackers and impression trackers (if applicable).</p>
+        
+        <h3>Animation Length</h3>
+        <p>30 seconds max</p>
+
+        <h3>Expandable Size</h3>
+        <p>Full screen responsive</p>
+        
+        <h3>File Size Limit</h3>
+        <p>60kb .jpg/.gif</p>
+        
+        <h3>Video can be included?</h3>
+        <p>Yes - will autoplay with sound muted</p>
+
+        <h3>Note:</h3>
+        <ul>
+          <li>PSD - Should be Layered.</li>
+          <li>Fonts - .ttf or .otf format.</li> 
+          <li>Store location list - applicable if creative has map page.</li>
+          <li>Video - Applicable if creative has video and MP4/MOV/AVI file formats accepted</li>
+          <li>Gif file if applicable</li>
+          <li>Third party Click trackers and impression trackers (if applicable)</li>
+          <li>Video can be included in-banner and expandable version</li>
+          <li>Expanded state may contain multiple pages</li>
+          <li>Each page can contain a gallery of images/videos</li>
+          <li>Each product in carousel can have single or multiple click trackers</li>
+          <li>Interstitial build will be responsive to device size. (Please consult Creative team for best practises.)</li>
+        </ul>
+
+
+
+
+
+
+
+
+
+
+
+        <h2>300x250 (Mrec)</h2>
+        <h3>Creative Size</h3>
+        <p>320x50 (Mobile Banner)</p>
+
+        <h3>PSD Assets dimensions to be supplied from client</h3>
+        <p>640x100</p>
+
+        <h3>Assets required</h3>
+        <p>PSD, Fonts, Storyboard. Third party click trackers and impression trackers (if applicable).</p>
+        
+        <h3>Animation Length</h3>
+        <p>30 seconds max</p>
+
+        <h3>Expandable Size</h3>
+        <p>Full screen responsive</p>
+        
+        <h3>File Size Limit</h3>
+        <p>60kb .jpg/.gif</p>
+        
+        <h3>Video can be included?</h3>
+        <p>Yes - will autoplay with sound muted</p>
+
+        <h3>Note:</h3>
+        <ul>
+          <li>PSD - Should be Layered.</li>
+          <li>Fonts - .ttf or .otf format.</li> 
+          <li>Store location list - applicable if creative has map page.</li>
+          <li>Video - Applicable if creative has video and MP4/MOV/AVI file formats accepted</li>
+          <li>Gif file if applicable</li>
+          <li>Third party Click trackers and impression trackers (if applicable)</li>
+          <li>Video can be included in-banner and expandable version</li>
+          <li>Expanded state may contain multiple pages</li>
+          <li>Each page can contain a gallery of images/videos</li>
+          <li>Each product in carousel can have single or multiple click trackers</li>
+          <li>Interstitial build will be responsive to device size. (Please consult Creative team for best practises.)</li>
+        </ul>
+      </div>
+    </div>
+    
   </div>
 )
 
